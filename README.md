2e30cf9ef3223d7cb64e585ccfc6d1c273ac5b44
Luiza Guerra <luiza.guerra@gmail.com>
2017-10-19 09:49:41 -0200

    adding files spec.js and confi.js

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # first-steps-protractor-web
 This is a simple tutorial that shows you how to set up Protractor and start running tests.
+
+# Setup
+Use npm to install Protractor globally with:
+```
+npm install -g protractor
+```
+This will install two command line tools, `protractor` and `webdriver-manager`. Try running `protractor --version` to make sure it's working.
+
+The `webdriver-manager` is a helper tool to easily get an instance of a Selenium Server running. Use it to download the necessary binaries with:
+```
+webdriver-manager update
+```
+Now start up a server with:
+```
+webdriver-manager start
+```
+
+This will start up a Selenium Server and will output a bunch of info logs. Your Protractor test will send requests to this server to control a local browser. Leave this server running throughout the tutorial. You can see information about the status of the server at http://localhost:4444/wd/hub.
---
+++ spec.js
@@ -0,0 +1,54 @@
+// spec.js
+describe('Protractor Demo App', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
+
+  beforeEach(function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+    browser.driver.manage().window().maximize();
+  });
+
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+
+  it('should add one and two', function() {
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six', function() {
+    firstNumber.sendKeys(4);
+    secondNumber.sendKeys(6);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('10');
+  });
+
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
+
+    expect(history.count()).toEqual(2);
+
+    add(5, 6);
+
+    expect(history.count()).toEqual(3); // This is wrong!
+  });
+
+
+});
