0cfef6a24f0d198b4d988dec32b559c81b9ccf0b
Luiza Guerra <luiza.guerra@gmail.com>
2017-10-23 17:14:12 -0200

    setting jasmine-spec-reporter

--- conf.js
+++ conf.js
@@ -1,6 +1,18 @@
 // conf.js
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['locators_spec.js']
+  specs: ['locators_spec.js'],
+
+  onPrepare: function(){
+    jasmine.getEnv().addReporter(new SpecReporter({
+      displayFailuresSummary: true,
+      displayFailuredSpec: true,
+      displaySuiteNumber: true,
+      displaySpecDuration: true
+    }));
+  }
+
 }
