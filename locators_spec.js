8027ebd661b4ea83459e5f4b573a21741f4c27af
Luiza Guerra <luiza.guerra@gmail.com>
2017-10-23 16:05:31 -0200

    using locators

--- locators_spec.js
+++ locators_spec.js
@@ -4,16 +4,6 @@ describe('Locators', function(){
     browser.driver.manage().window().maximize();
   });
 
-  it('should locate an element by css', function() {
-    var secondField = element.all(by.css('.input-small.ng-pristine.ng-untouched.ng-valid')).get(1);
-    expect(secondField.isPresent()).toBe(true);
-  });
-
-  it('should locate an element by id', function() {
-    var goButton = element(by.id('gobutton'));
-    expect(goButton.isPresent()).toBe(true);
-  });
-
   it('should locate an element by model', function() {
     var firstField = element(by.model('first'));
     expect(firstField.isPresent()).toBe(true);
@@ -22,6 +12,28 @@ describe('Locators', function(){
   it('should locate an element by binding', function() {
     var latestResult = element(by.binding('latest'));
     expect(latestResult.isPresent()).toBe(true);
+  });
+
+  it('should locate an element by repeater', function() {
+    var history = element.all(by.repeater('result in memory'));
+
+    var firstField = element(by.model('first'));
+    var secondField = element.all(by.css('.input-small.ng-pristine.ng-untouched.ng-valid')).get(0);
+    var goButton = element(by.id('gobutton'));
+    firstField.sendKeys(2);
+    secondField.sendKeys(3);
+    goButton.click();
+    expect(history.count()).toEqual(1);
+  });
+
+  it('should locate an element by css', function() {
+    var secondField = element.all(by.css('.input-small.ng-pristine.ng-untouched.ng-valid')).get(1);
+    expect(secondField.isPresent()).toBe(true);
+  });
+
+  it('should locate an element by id', function() {
+    var goButton = element(by.id('gobutton'));
+    expect(goButton.isPresent()).toBe(true);
   });
 
   it('should locate elements by options', function() {
